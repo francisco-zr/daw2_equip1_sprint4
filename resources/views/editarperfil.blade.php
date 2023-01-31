3064e3bb547cedae3af4fbcc4a686547f6f53577
TatianaValentiny <tatianavalentinyova@iesmontsia.org>
2023-01-31 21:01:15 +0100

    editar perfil

--- resources/views/editarperfil.blade.php
+++ resources/views/editarperfil.blade.php
@@ -30,7 +30,6 @@
        <input type="text" name="name" id="name" class="form-control" value="Tatiana17_7">
    </div>
 
-
    <div class="form-group">
        <i class="fas fa-envelope"></i>
        <label for="name">Email</label>
@@ -53,13 +52,31 @@
 
 
    <!-- Agrega otros campos aquí -->
-  
+   <style>
+  .btn-refresh {
+    position: relative;
+  }
 
+  .btn-refresh .fa-sync-alt {
+    -webkit-transition: all 0.5s ease-in-out;
+    -moz-transition: all 0.5s ease-in-out;
+    -ms-transition: all 0.5s ease-in-out;
+    -o-transition: all 0.5s ease-in-out;
+    transition: all 0.5s ease-in-out;
+  }
 
-   <button type="submit" class="btn btn-primary">Actualizar</button>
-  
+  .btn-refresh:hover .fa-sync-alt {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+</style>
 
-
+<button class="btn btn-primary btn-refresh">
+  <i class="fas fa-sync-alt"></i> Actualizar
+</button>
 </form>
 
 
