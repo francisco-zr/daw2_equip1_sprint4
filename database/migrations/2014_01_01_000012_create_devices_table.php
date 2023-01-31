9f7ed7acb1ce2a180548893682ada771f332d60d
fran <franciscozahinos@iesmontsia.org>
2023-01-31 20:54:02 +0100

    migrations primera parte

---
+++ database/migrations/2014_01_01_000012_create_devices_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand');
+            $table->string('model');
+            $table->string('mac_ethernet')->nullable();
+            $table->string('mac_wifi')->nullable();
+            $table->text('description')->nullable();
+            $table->string('state')->nullable();
+            $table->string('tag')->nullable();
+            $table->string('serial_number');
+            $table->foreignId('type_device_id')->constrained();
+            $table->date('hidden')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+};
