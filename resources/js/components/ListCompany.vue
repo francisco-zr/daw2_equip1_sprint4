687bdb67ea3884a8096a4d6f5bb4344b45bca95b
PolObalat <polobalat@iesmontsia.org>
2023-02-06 16:10:04 +0100

    llistatEmpresesVue

--- resources/js/components/ListCompany.vue
+++ resources/js/components/ListCompany.vue
@@ -1,3 +1,6 @@
+Vue.component('ListCompany,{
+    template:
+}')
 <template>
     <div class="m-10 relative overflow-x-auto">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
