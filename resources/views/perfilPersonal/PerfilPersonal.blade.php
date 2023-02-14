8d035ad58cdcf0f8c22ebac2003d16ee29b02ac5
TatianaValentiny <tatianavalentinyova@iesmontsia.org>
2023-02-14 19:07:49 +0100

    perfil

--- resources/views/perfilPersonal/PerfilPersonal.blade.php
+++ resources/views/perfilPersonal/PerfilPersonal.blade.php
@@ -6,73 +6,89 @@
     @endpush
     <div class="container mx-auto flex flex-wrap">
         <div class="w-1/2 p-6" id="principal">
-
-            <img src="/img/imatgeTatiana.png" alt="imagen_del_usuario" class="mb-3 rounded-full w-48 h-48" id="block">
-            <h4 id="block" class="text-2xl font-bold">{{ $authenticatedUser->name }}</h4>
+            
+            <img src="{{ $authenticatedUser->profile_image ? $authenticatedUser->profile_image : asset('img/default_profile.png') }}" alt="imagen_del_usuario" class="mb-3 rounded-full w-48 h-48" id="block">
+            <h4 id="block1" class="text-2xl font-bold">{{ $authenticatedUser->name }} {{ $authenticatedUser->last_name }}</h4>
         </div>
         <div class="w-1/2 p-6">
             <table id="tablePerfil" class="table-auto text-lg">
                 <tbody>
                     <tr class="bg-gray-100">
-                        <td class="font-bold pr-4">Nombre:</td>
+                        <td>Nombre</td>
                         <td>{{ $authenticatedUser->name }}</td>
                     </tr>
                     <tr>
-                        <td class="font-bold pr-4">Apellidos:</td>
+                        <td>Apellidos</td>
                         <td>{{ $authenticatedUser->last_name }}</td>
                     </tr>
                     <tr class="bg-gray-100">
-                        <td class="font-bold pr-4">Nombre de usuario:</td>
+                        <td>Nombre de usuario</td>
                         <td>{{ $authenticatedUser->nick_name }}
                         </td>
                     </tr>
                     <tr>
-                        <td class="font-bold pr-4">Email:</td>
+                        <td>Email</td>
                         <td>{{ $authenticatedUser->email }}</td>
                     </tr>
                     <tr class="bg-gray-100">
-                        <td class="font-bold pr-4">Teléfono:</td>
+                        <td>Teléfono</td>
                         <td>{{ $authenticatedUser->phone }}</td>
                     </tr>
                     <tr>
-                        <td class="font-bold pr-4">Empresa</td>
+                        <td>Empresa</td>
                         <td>{{ $authenticatedUser->company_id }}</td>
                     </tr>
                 </tbody>
             </table>
 
             <div class="mt-6">
-                <a href="/Perfil_Personal/Editar_Perfil" class="boton1">Editar Perfil</a>
+                <a href="/Perfil_Personal/Editar_Perfil" class="bg-orange-500 hover:bg-white text-black font-medium py-2 px-4 rounded-lg border-2 border-black transition-transform duration-500 ease-in-out">
+                    Editar Perfil
+                  </a>
+                  
             </div>
         </div>
     </div>
 @endsection
 <style scoped>
-    .boton1 {
-        background-color: rgb(247, 192, 115);
-        color: rgb(0, 0, 0);
-        border: 2px solid rgb(0, 0, 0);
-        border-radius: 5px;
-        font-size: 16px;
-        padding: 10px 20px;
-        margin-left: 10px;
-    }
-
-    .boton1:hover {
-        background-color: white;
-        color: rgb(0, 0, 0);
-        cursor: pointer;
-        transform: scale(1.1);
-    }
 
     #block {
-        margin-left: 350px;
+        margin-left: 300px;
 
     }
+    #block1 {
+        margin-left: 270px;
 
+    }
     #tablePerfil {
-        height: 300px;
+        height: 250px;
         width: 500px;
 
     }
+
+    
+    h4 {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+
+    td:first-child {
+        padding-right: 20px;
+    }
+
+    td:last-child {
+        padding-left: 20px;
+    }
+
+    tr:nth-child(odd) {
+        /*Ficar color a les linies inpars*/
+        background-color: lightgray;
+    }
+
+    tr:nth-child(even) {
+        /*Ficar color a les linies pars*/
+        background-color: white;
+    }
+    
 </style>
